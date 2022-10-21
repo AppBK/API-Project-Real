4f21a6d91481f485c4c28f4a9cb6fca96af5c6ae
Brian Kiesel <app_academy_student_bk@protonmail.com>
2022-10-20 20:04:32 -0700

    Add User signup backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -25,15 +25,23 @@ router.post('/', async (req, res, next) => {
   });
 });
 
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
 module.exports = router;
 
+// "TtcRABgi-0fR6k8AblItQlH6GWRKkw4H1VXs"
+// Google: "cm9MVmzx-prc8fEdSlDtRCWS5NUPhN2o83TM"
 
 /*
 fetch('/api/session', {
   method: 'POST',
   headers: {
     "Content-Type": "application/json",
-    "XSRF-TOKEN": "in5tlx7f--9ogxOhxMPi1nnGa48SZcie2qc4"
+    "XSRF-TOKEN": "cm9MVmzx-prc8fEdSlDtRCWS5NUPhN2o83TM"
   },
   body: JSON.stringify({ credential: 'Demo-lition', password: 'password' })
 }).then(res => res.json()).then(data => console.log(data));
@@ -59,4 +67,15 @@ fetch('/api/session', {
   },
   body: JSON.stringify({ credential: 'Demo-lition', password: 'Hello World!' })
 }).then(res => res.json()).then(data => console.log(data));
+*/
+
+/*
+fetch('/api/session', {
+  method: 'DELETE',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": "cm9MVmzx-prc8fEdSlDtRCWS5NUPhN2o83TM"
+  }
+}).then(res => res.json()).then(data => console.log(data));
+
 */
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,65 @@
-const express = require('express')
+const express = require('express');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
+router.post('/', async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user
+    });
+  }
+);
+
 module.exports = router;
+
+/*
+
+fetch('/api/users', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": "cm9MVmzx-prc8fEdSlDtRCWS5NUPhN2o83TM"
+  },
+  body: JSON.stringify({
+    email: 'spidey@spider.man',
+    username: 'Spidey',
+    password: 'password'
+  })
+}).then(res => res.json()).then(data => console.log(data));
+
+fetch('/api/users', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": "cm9MVmzx-prc8fEdSlDtRCWS5NUPhN2o83TM"
+  },
+  body: JSON.stringify({
+    email: 'poser@io.net',
+    username: 'Spidey',
+    password: 'password'
+  })
+}).then(res => res.json()).then(data => console.log(data));
+
+fetch('/api/users', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": "cm9MVmzx-prc8fEdSlDtRCWS5NUPhN2o83TM"
+  },
+  body: JSON.stringify({
+    email: 'spidey@spider.man',
+    username: 'poser',
+    password: 'password'
+  })
+}).then(res => res.json()).then(data => console.log(data));
+
+
+
+*/
