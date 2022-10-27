18ef88f6769cead08aa08fe345550447cf4928cf
Brian Kiesel <app_academy_student_bk@protonmail.com>
2022-10-26 17:38:33 -0700

    added endpoint for Get all of the Current Users Bookings in /api/bookings.js

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
-const { Spot, SpotImage, Review, ReviewImage, User, Sequelize } = require('../../db/models');
+const { Spot, SpotImage, Review, ReviewImage, User, Booking, Sequelize } = require('../../db/models');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
@@ -23,7 +23,35 @@ const router = express.Router();
 
 // Get all of the Current User's Bookings
 router.get('/current', [restoreUser, requireAuth], async (req, res) => {
-  ;
+  let bookings = await Booking.findAll({ where: { userId: req.user.id } });
+
+  console.log(bookings);
+
+  const response = [];
+  if (bookings.length) {
+    for (let i = 0; i < bookings.length; i++) {
+      // Get raw data object from booking
+      bookings[i] = bookings[i].dataValues;
+
+      // Get spot data to add to response object
+      let spot = await Spot.findByPk(bookings[i].spotId);
+      spot = spot.dataValues;
+      delete spot.description;
+      delete spot.createdAt;
+      delete spot.updatedAt;
+
+      let spotImg = await SpotImage.findOne({ where: { spotId: spot.id, preview: true }});
+      spotImg = spotImg.dataValues;
+
+      spot.previewImage = spotImg.url;
+
+      bookings[i].Spot = spot;
+
+      response.push(bookings[i]);
+    }
+  }
+
+  return res.json({ "Bookings": response });
 });
 
 
