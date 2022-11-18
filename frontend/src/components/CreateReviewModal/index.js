c6cbaaf6b927e57c00508164b3f30b5ad8558a6c
Brian Kiesel <app_academy_student_bk@protonmail.com>
2022-11-17 22:13:11 -0800

    not bad

--- frontend/src/components/CreateReviewModal/index.js
+++ frontend/src/components/CreateReviewModal/index.js
@@ -3,9 +3,11 @@ import { useState, useEffect, useContext } from 'react';
 import { RouterContext } from '../../context/RouterContext';
 import '../../context/Modal.css';
 import CreateReview from "./CreateReview";
+import { ModalContext } from "../../context/Modal";
 
 export default function CreateReviewModal({ spot }) {
   const { showCreateReview, setShowCreateReview } = useContext(RouterContext);
+  const { setShowModal } = useContext(ModalContext);
 
   return (
     <div id='generic'>
